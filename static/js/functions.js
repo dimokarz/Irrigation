51fb92315c2e577d85ef31d072a81dd4299add7d
di_mok <dimokarz@yandex.ru>
2022-08-11 19:06:56 +0300

    !!!

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,33 +1,19 @@
-// Проверка выбранных систем
-function btn_sel() {
-    let duet = [];
-    let valley = [];
-    let inputElems = document.getElementsByTagName("input");
-    let count = 0;
-
-    for (let i = 0; i < inputElems.length; i++) {
-        if (inputElems[i].type == "checkbox" && inputElems[i].checked == true) {
-            count++;
-            duet.push(inputElems[i].value);
-            valley.push(inputElems[i].id);
-        }
-    }
-    switch (count) {
-        case 1:
-            window.open("/simple?first=" + valley[0], "_self");
+$(document).ready(function() {
+    switch (document.location.pathname) {
+        case '/login/':
+            $('body').css('background-size', 'cover');
+            $('body').css('background-image', 'url(/static/img/valley.jpg)');
             break;
-        case 2:
-            if (duet[0] == duet[1] && duet[0] != 0) {
-                window.open("/simple?first=" + valley[0] + "&second=" + valley[1], "_self");
-            }
-            else {
-                $('#mal1').text("Выбранные системы не могут работать в паре");
-                $('#modAlert').modal("show");
-            }
+        case '/simple/':
+            $('body').css('background-image', '');
             break;
         default:
-            $('#mal1').text("Вы можете выбрать не более двух систем");
-            $('#modAlert').modal("show");
+            $('body').css('background-image', '');
+            $.getJSON('/states_list', function (data){
+                for (key in data) {
+                    alert(data[key].states_ctrl)
+                }
+            });
             break;
     }
-}
+});
