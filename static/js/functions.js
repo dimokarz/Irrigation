7b108af269e96ab805e42009a18d005e849bf432
Di_Mok <dimokarz@yandex.ru>
2022-08-17 17:02:49 +0300

    Index

--- static/js/functions.js
+++ static/js/functions.js
@@ -9,10 +9,33 @@ $(document).ready(function() {
             break;
         default:
             $('body').css('background-image', '');
+            //Статусы систем
             $.getJSON('/states_list', function (data){
-                for (key in data) {
-                    alert(data[key].states_ctrl)
+                for (let i = 0; i < 2; i++) {
+                    for (let y = 0; y <= data.length; y++) {
+                        $('#spState' + y).fadeTo('slow', 0.0).fadeTo('slow', 1.0);
+                    }
                 }
+                setTimeout(function () {
+                    for (key in data) {
+                        $('#spState' + key).removeClass('bg-warning');
+                        $('#spState' + key).removeClass('text-dark');
+                        if (data[key].states_ctrl == false) {
+                            $('#spState' + key).addClass('bg-secondary');
+                            $('#spState' + key).text('Не доступна');
+                        }
+                        else {
+                            if (data[key].states_dir == 'N') {
+                                $('#spState' + key).addClass('bg-danger');
+                                $('#spState' + key).text('Остановлена');
+                            }
+                            else {
+                                $('#spState' + key).addClass('bg-success');
+                                $('#spState' + key).text('Запущена');
+                            }
+                        }
+                    }
+                }, 2000);
             });
             break;
     }
